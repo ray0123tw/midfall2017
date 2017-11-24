8de4af0ade2a81a0f94a84b6107ae1b9856f46b0
ray0123tw <ray0123tw@yahoo.com.tw>
2017-11-24 14:28:56 +0800

    1124-1

---
+++ htdocs/js/sokoban.js
@@ -0,0 +1,19 @@
+/**
+ *  @file       index.js
+ *  @brief      The entry file of Sokoban.
+ *  @author     ray0123tw (ray0123tw@yahoo.com.tw)
+ *  @date       11/17/2017 created.
+ *  @date       11/24/2017 last modified.
+ *  @version    0.1.0
+ *  @copyright  MIT, (C) 2017 ray0123tw
+ *  @details
+ *
+ *  The entry file of Sokoban.
+ */
+'use strict';
+
+window.addEventListener('load', () => {
+  console.log("Sokoban.js loaded");
+});
+
+// index.js
